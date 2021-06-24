cb6d9f3be07f8f9ec6acb532afa61832bdfc58e4
Andrew Bone <AndrewB05@gmail.com>
2021-06-24 16:33:06 +0100

    Execute function
    Execute function before initial return

--- src/expect.js
+++ src/expect.js
@@ -1,88 +1,60 @@
-const { prettyPrint, areSameObjectProperties } = require('./utilities');
+const { prettyPrint, areSameObjectProperties } = require("./utilities");
 
 // expect(something).toBe(something2);
 
-function toBe(actualValue) {
-  return function (expectedValue) {
-    if (Object.is(expectedValue, actualValue)) {
-      console.log('✅');
-    } else {
-      console.error(
-        `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-          expectedValue,
-        )}`,
-      );
-    }
-  };
-}
-
-function toEqual(actualValue) {
-  return function (expectedValue) {
-    if (typeof expectedValue === 'object') {
-      if (areSameObjectProperties(expectedValue, actualValue)) {
-        console.log('✅');
-      } else {
-        console.error(
-          `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-            expectedValue,
-          )}`,
-        );
-      }
-    } else {
-      if (expectedValue == actualValue) {
-        console.log('✅');
-      } else {
-        console.error(
-          `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-            expectedValue,
-          )}`,
-        );
-      }
-    }
-  };
-}
-
 function expect(actualValue) {
-  return {
-    not: {
+  const functions = (invert) => {
+    return {
       toBe(expectedValue) {
-        if (!Object.is(expectedValue, actualValue)) {
-          console.log('✅');
+        let test = Object.is(expectedValue, actualValue);
+        if (invert) test = !test;
+
+        if (test) {
+          console.log("✅");
         } else {
           console.error(
             `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-              expectedValue,
-            )}`,
+              expectedValue
+            )}`
           );
         }
       },
       toEqual(expectedValue) {
-        if (typeof expectedValue === 'object') {
-          if (!areSameObjectProperties(expectedValue, actualValue)) {
-            console.log('✅');
+        let test;
+        if (typeof expectedValue === "object") {
+          test = areSameObjectProperties(expectedValue, actualValue);
+          if (invert) test = !test;
+
+          if (test) {
+            console.log("✅");
           } else {
             console.error(
               `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-                expectedValue,
-              )}`,
+                expectedValue
+              )}`
             );
           }
         } else {
-          if (expectedValue != actualValue) {
-            console.log('✅');
+          test = expectedValue == actualValue;
+          if (invert) test = !test;
+
+          if (test) {
+            console.log("✅");
           } else {
             console.error(
               `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-                expectedValue,
-              )}`,
+                expectedValue
+              )}`
             );
           }
         }
       },
-    },
-    toBe: toBe(actualValue),
-    toEqual: toEqual(actualValue),
+    };
   };
+
+  functions.not = functions(true);
+
+  return functions(false);
 }
 
 module.exports = {
