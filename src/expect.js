e2623dc93af89b355a47fbd38d9d96823091a26d
Nick Taylor <nick@iamdeveloper.com>
2021-06-24 11:30:43 -0400

    Refactored .toBe and .toEqual.

--- src/expect.js
+++ src/expect.js
@@ -2,6 +2,46 @@ const { prettyPrint, areSameObjectProperties } = require('./utilities');
 
 // expect(something).toBe(something2);
 
+function toBe(actualValue) {
+  return function (expectedValue) {
+    if (Object.is(expectedValue, actualValue)) {
+      console.log('✅');
+    } else {
+      console.error(
+        `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
+          expectedValue,
+        )}`,
+      );
+    }
+  };
+}
+
+function toEqual(actualValue) {
+  return function (expectedValue) {
+    if (typeof expectedValue === 'object') {
+      if (areSameObjectProperties(expectedValue, actualValue)) {
+        console.log('✅');
+      } else {
+        console.error(
+          `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
+            expectedValue,
+          )}`,
+        );
+      }
+    } else {
+      if (expectedValue == actualValue) {
+        console.log('✅');
+      } else {
+        console.error(
+          `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
+            expectedValue,
+          )}`,
+        );
+      }
+    }
+  };
+}
+
 function expect(actualValue) {
   return {
     not: {
@@ -40,40 +80,8 @@ function expect(actualValue) {
         }
       },
     },
-    toBe(expectedValue) {
-      if (Object.is(expectedValue, actualValue)) {
-        console.log('✅');
-      } else {
-        console.error(
-          `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-            expectedValue,
-          )}`,
-        );
-      }
-    },
-    toEqual(expectedValue) {
-      if (typeof expectedValue === 'object') {
-        if (areSameObjectProperties(expectedValue, actualValue)) {
-          console.log('✅');
-        } else {
-          console.error(
-            `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-              expectedValue,
-            )}`,
-          );
-        }
-      } else {
-        if (expectedValue == actualValue) {
-          console.log('✅');
-        } else {
-          console.error(
-            `❌ expected ${prettyPrint(actualValue)} to equal ${prettyPrint(
-              expectedValue,
-            )}`,
-          );
-        }
-      }
-    },
+    toBe: toBe(actualValue),
+    toEqual: toEqual(actualValue),
   };
 }
 
